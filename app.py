ebca34666af70e67f576f792e630a8ff8c04aa23
tlsehdakdrh <sind5769@naver.com>
2024-12-25 21:22:21 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -139,3 +140,5 @@ def get_element():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
